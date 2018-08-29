edeacdfd44d42a8489458d24db586b9821691187
Nuno <yo_325@yahoo.com>
2018-08-29 14:14:20 +0100

    Part.5 - Angular 2 Componentes & Rotas (não segui BookStore-Tuto - instalei apenas o bower)
    download angular-cli:
    	// na pasta do projecto: restful-app
    	. npm install -g angular-cli
    	// criar pasta angular-src: tem toda a estrutura de angular-2
    	. ng new angular-src
    ::
    mudar angula-cli.json
    	// mudar o output direcction
    	. quando fazemos ng build e compila, quero que vá para pasta \public (min.2.30)
    /+ ::
    instalar bower (pertence ao tutorial bookstore-part_2(min.6.00))
    	// na pasta do projecto: restful-app
    	. npm install -g bower
    	// não fiz mais nada - instalei apenas o bower
    :: +/
    ::
    criar componente:
    	. min.06.00
    ::
    incluir bootstrap:
    	// https://bootswatch.com/
    	. min. 10.30
    	// https://getbootstrap.com/docs/4.1/examples/
    	// https://getbootstrap.com/docs/4.1/examples/starter-template/  ++ ctrl+u: ver src-code
    	. min. 16.00

--- RESTful-App/angular-src/src/app/app.module.ts
+++ RESTful-App/angular-src/src/app/app.module.ts
@@ -2,17 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+// REF: \AUTH\.5\(min.11.30)
+import {RouterModule, Routes} from '@angular/router';
+// array de rotas
+const appRoutes: Routes = [
+  // path:'' - porque é home
+  {path:'', component:HomeComponent},
+  {path:'register', component:RegisterComponent},
+  {path:'login', component:LoginComponent},
+  {path:'dashboard', component:DashboardComponent},
+  {path:'profile', component:ProfileComponent}
+]
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
